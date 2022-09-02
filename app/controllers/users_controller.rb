dfc60064abd0ce2e18bc5c64eb6ef3dc81b75c49
Margaux Pichard <margauxpichard@gmail.com>
2022-09-02 17:46:28 +0200

    page mon compte mes préférences

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,7 @@ class UsersController < ApplicationController
     @my_leagues = League.where_am_i(current_user)
     @my_admissions = Admission.includes(:league).where(league: {user_id: current_user}, status: "pending")
   end
+
+  def account
+  end
 end
