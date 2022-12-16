f3a70c102828e213b9d40a5464d0469566aa04fa
cedric le brun <lebruncedric8@gmail.com>
2022-12-16 10:32:15 +0100

    add webpack.config.js + update js stimulus controller and fix bug

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,9 +1,34 @@
-// Load all the controllers within this directory and all subdirectories. 
-// Controller files must be named *_controller.js.
+// Import and register all your controllers from the importmap under controllers/*
 
-import { Application } from "stimulus"
-import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { application } from "./application"
 
-const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
+// Eager load all controllers defined in the import map under controllers/**/*_controller
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// eagerLoadControllersFrom("controllers", application)
+
+// Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
+// import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
+// lazyLoadControllersFrom("controllers", application)
+import CloseButtonController from "./close_button_controller"
+application.register("close-button", CloseButtonController)
+
+import CountdownRaceController from "./countdown_race_controller"
+application.register("countdown-race", CountdownRaceController)
+
+import CountdownStageController from "./countdown_stage_controller"
+application.register("countdown-stage", CountdownStageController)
+
+import DropdownButtonController from "./dropdown_button_controller"
+application.register("dropdown-button", DropdownButtonController)
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
+
+import PelotonPopupController from "./peloton_popup_controller"
+application.register("peloton-popup", PelotonPopupController)
+
+import PronoPopupController from "./prono_popup_controller"
+application.register("prono-popup", PronoPopupController)
+
+import ShowLeagueButtonController from "./show_league_button_controller"
+application.register("show-league-button", ShowLeagueButtonController)
