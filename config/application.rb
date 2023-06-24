62f1c32ad86bdbe308bd1fbcdaa2781f49665e76
cedric le brun <lebruncedric8@gmail.com>
2023-06-24 11:17:39 +0200

    Config time zone

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module PronosVelo
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = 'e'
+    config.time_zone = 'Paris'
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
