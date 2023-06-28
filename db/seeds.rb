71d1c49297cc10abbc33b6adc68889fa076cd254
cedric le brun <lebruncedric8@gmail.com>
2023-06-28 18:54:35 +0200

    Update seeds for Tour de France 2023

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,33 +26,6 @@ race1.save!
 
 puts "Creating races done"
 
-puts "Creating the users"
-
-user1 = User.new(
-  first_name: 'Margaux',
-  last_name: 'Pichard',
-  username: 'marpich',
-  email: 'margaux@gmail.com',
-  password: 'margaux',
-  admin: true
-)
-photomarpich = URI.open('https://res.cloudinary.com/dciokrtia/image/upload/v1653653443/photo-margaux_gr8cjn.jpg')
-user1.photo.attach(io: photomarpich, filename: 'marpich.png', content_type: 'image/png')
-user1.save!
-
-user2 = User.new(
-  first_name: 'Cedric',
-  last_name: 'Le Brun',
-  username: 'cedleb',
-  email: 'cedric@gmail.com',
-  password: 'cedric'
-)
-photocedleb = URI.open('https://res.cloudinary.com/dciokrtia/image/upload/v1653653052/photo-c%C3%A9dric_h6m7dd.jpg')
-user2.photo.attach(io: photocedleb, filename: 'cedleb.jpg', content_type: 'image/jpg')
-user2.save!
-
-puts "Creating users done"
-
 puts "Creating the stages"
 
 CSV.foreach(filepath, headers: :first_row) do |row|
@@ -73,15 +46,13 @@ end
 puts "Creating stages done"
 
 puts "Creating the riders"
-i = 1
 CSV.foreach(filepath_riders, headers: :first_row) do |row|
   rider = Rider.new(
     last_name: row['last_name'],
     first_name: row['first_name'],
     team: row['team'],
-    bib: i
+    bib: row[0]
   )
-  i += 1
   # url = URI("https://pro-cycling-stats.p.rapidapi.com/riders/#{row['first_name'].parameterize}-#{row['last_name'].parameterize}")
 
   # http = Net::HTTP.new(url.host, url.port)
@@ -101,167 +72,3 @@ CSV.foreach(filepath_riders, headers: :first_row) do |row|
 end
 
 puts "Creating riders done"
-
-puts "Creating the bets"
-
-# bet11 = Bet.new(
-#   user: user1,
-#   stage: Stage.first,
-#   position: 1,
-#   rider: Rider.first
-# )
-# bet11.save!
-
-# bet12 = Bet.new(
-#   user: user1,
-#   stage: Stage.first,
-#   position: 2,
-#   rider: Rider.second
-# )
-# bet12.save!
-
-# bet13 = Bet.new(
-#   user: user1,
-#   stage: Stage.first,
-#   position: 3,
-#   rider: Rider.third
-# )
-# bet13.save!
-
-# bet21 = Bet.new(
-#   user: user1,
-#   stage: Stage.second,
-#   position: 1,
-#   rider: Rider.first
-# )
-# bet21.save!
-
-# bet22 = Bet.new(
-#   user: user1,
-#   stage: Stage.second,
-#   position: 2,
-#   rider: Rider.second
-# )
-# bet22.save!
-
-# bet23 = Bet.new(
-#   user: user1,
-#   stage: Stage.second,
-#   position: 3,
-#   rider: Rider.third
-# )
-# bet23.save!
-
-# bet31 = Bet.new(
-#   user: user1,
-#   stage: Stage.third,
-#   position: 1,
-#   rider: Rider.first
-# )
-# bet31.save!
-
-# bet32 = Bet.new(
-#   user: user1,
-#   stage: Stage.third,
-#   position: 2,
-#   rider: Rider.second
-# )
-# bet32.save!
-
-# bet33 = Bet.new(
-#   user: user1,
-#   stage: Stage.third,
-#   position: 3,
-#   rider: Rider.third
-# )
-# bet33.save!
-
-# bet41 = Bet.new(
-#   user: user2,
-#   stage: Stage.first,
-#   position: 1,
-#   rider: Rider.third
-# )
-# bet41.save!
-
-# bet42 = Bet.new(
-#   user: user2,
-#   stage: Stage.first,
-#   position: 2,
-#   rider: Rider.second
-# )
-# bet42.save!
-
-# bet43 = Bet.new(
-#   user: user2,
-#   stage: Stage.first,
-#   position: 3,
-#   rider: Rider.first
-# )
-# bet43.save!
-
-# bet51 = Bet.new(
-#   user: user2,
-#   stage: Stage.second,
-#   position: 1,
-#   rider: Rider.third
-# )
-# bet51.save!
-
-# bet52 = Bet.new(
-#   user: user2,
-#   stage: Stage.second,
-#   position: 2,
-#   rider: Rider.second
-# )
-# bet52.save!
-
-# bet53 = Bet.new(
-#   user: user2,
-#   stage: Stage.second,
-#   position: 3,
-#   rider: Rider.first
-# )
-# bet53.save!
-
-# bet61 = Bet.new(
-#   user: user2,
-#   stage: Stage.third,
-#   position: 1,
-#   rider: Rider.third
-# )
-# bet61.save!
-
-# bet62 = Bet.new(
-#   user: user2,
-#   stage: Stage.third,
-#   position: 2,
-#   rider: Rider.second
-# )
-# bet62.save!
-
-# bet63 = Bet.new(
-#   user: user2,
-#   stage: Stage.third,
-#   position: 3,
-#   rider: Rider.first
-# )
-# bet63.save!
-
-puts "Creating bets done"
-
-puts "Creating the total scores"
-
-total_score1 = TotalScore.new(
-  race: race1,
-  user: user1
-)
-total_score1.save!
-
-total_score2 = TotalScore.new(
-  race: race1,
-  user: user2
-)
-total_score2.save!
-
-puts "Creating total scores done"
