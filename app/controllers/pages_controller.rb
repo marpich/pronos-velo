89ed8c66f0fc714e7ebc15b52b6b84fe4cc6e4c7
cedric le brun <lebruncedric8@gmail.com>
2023-06-30 10:36:30 +0200

    Update home page if Race not started

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @race = Race.first
     @stage = Stage.where(date: Date.current).first
     @date = "#{@stage.date.strftime('%b')} #{@stage.date.strftime('%d')}, #{@stage.date.strftime('%Y')}" if @stage
     @rest_day = Stage.where(date: Date.current + 1).first
