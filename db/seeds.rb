579482d98a2f42f6afa5663206094993c00146a4
cedric le brun <lebruncedric8@gmail.com>
2023-08-25 13:20:34 +0200

    Set riders for vuelta 2023

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,65 +5,65 @@ require 'net/http'
 require 'openssl'
 require 'active_support/inflector'
 
-filepath = "db/fixtures/vuelta_stages.csv"
-# filepath_riders = "db/fixtures/riders.csv"
+# filepath = "db/fixtures/vuelta_stages.csv"
+filepath_riders = "db/fixtures/vuelta_riders.csv"
 
-puts "Creating the races"
+# puts "Creating the races"
 
-race1 = Race.new(
-  name: 'Vuelta 2023',
-  year: 2023,
-  starting_date: '26/08/2023',
-  ending_date: '17/09/2023'
-)
-race1.save!
+# race1 = Race.new(
+#   name: 'Vuelta 2023',
+#   year: 2023,
+#   starting_date: '26/08/2023',
+#   ending_date: '17/09/2023'
+# )
+# race1.save!
 
-puts "Creating races done"
+# puts "Creating races done"
 
-puts "Creating the stages"
+# puts "Creating the stages"
 
-CSV.foreach(filepath, headers: :first_row) do |row|
-  image_map = URI.open(row['image_map'])
-  stage = Stage.new(
-    number: row[0],
-    date: row['date'],
-    stage_type: row['stage_type'],
-    length: row['length'],
-    departure: row['departure'],
-    arrival: row['arrival']
-  )
-  stage.image.attach(io: image_map, filename: "stage#{row[0]}.png", content_type: 'image/png')
-  stage.race = race1
-  stage.save!
-end
+# CSV.foreach(filepath, headers: :first_row) do |row|
+#   image_map = URI.open(row['image_map'])
+#   stage = Stage.new(
+#     number: row[0],
+#     date: row['date'],
+#     stage_type: row['stage_type'],
+#     length: row['length'],
+#     departure: row['departure'],
+#     arrival: row['arrival']
+#   )
+#   stage.image.attach(io: image_map, filename: "stage#{row[0]}.png", content_type: 'image/png')
+#   stage.race = race1
+#   stage.save!
+# end
 
 puts "Creating stages done"
 
-# puts "Creating the riders"
-# CSV.foreach(filepath_riders, headers: :first_row) do |row|
-#   rider = Rider.new(
-#     last_name: row['last_name'],
-#     first_name: row['first_name'],
-#     team: row['team'],
-#     bib: row[0]
-#   )
-#   # url = URI("https://pro-cycling-stats.p.rapidapi.com/riders/#{row['first_name'].parameterize}-#{row['last_name'].parameterize}")
+puts "Creating the riders"
+CSV.foreach(filepath_riders, headers: :first_row) do |row|
+  rider = Rider.new(
+    last_name: row['last_name'],
+    first_name: row['first_name'],
+    team: row['team'],
+    bib: row[0]
+  )
+  # url = URI("https://pro-cycling-stats.p.rapidapi.com/riders/#{row['first_name'].parameterize}-#{row['last_name'].parameterize}")
 
-#   # http = Net::HTTP.new(url.host, url.port)
-#   # http.use_ssl = true
-#   # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  # http = Net::HTTP.new(url.host, url.port)
+  # http.use_ssl = true
+  # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-#   # request = Net::HTTP::Get.new(url)
-#   # request["X-RapidAPI-Key"] = ENV['API_KEY']
-#   # request["X-RapidAPI-Host"] = 'pro-cycling-stats.p.rapidapi.com'
+  # request = Net::HTTP::Get.new(url)
+  # request["X-RapidAPI-Key"] = ENV['API_KEY']
+  # request["X-RapidAPI-Host"] = 'pro-cycling-stats.p.rapidapi.com'
 
-#   # response = http.request(request)
-#   # infos = response.read_body
-#   # rider.team = JSON.parse(infos).first["Main info"]["team"]
-#   # rider.nationality = JSON.parse(infos).first["Main info"]["countryFlag"]
-#   # rider.photo_url = JSON.parse(infos).first["Main info"]["riders photo url"]
-#   rider.race = race1
-#   rider.save!
-# end
+  # response = http.request(request)
+  # infos = response.read_body
+  # rider.team = JSON.parse(infos).first["Main info"]["team"]
+  # rider.nationality = JSON.parse(infos).first["Main info"]["countryFlag"]
+  # rider.photo_url = JSON.parse(infos).first["Main info"]["riders photo url"]
+  rider.race = Race.last
+  rider.save!
+end
 
-# puts "Creating riders done"
+puts "Creating riders done"
