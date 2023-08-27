bc36770a4aee1a172294b6b377ab8f68f8211629
cedric le brun <lebruncedric8@gmail.com>
2023-08-27 12:24:00 +0200

    Update rider list for last race

--- app/models/rider.rb
+++ app/models/rider.rb
@@ -4,12 +4,12 @@ class Rider < ApplicationRecord
   belongs_to :race
 
   def self.get_all
-    Rider.where(still_racing: true, race: Race.first).sort_by(&:last_name).map {|rider| "#{rider.bib} - #{rider.last_name} #{rider.first_name}"}
+    Rider.where(still_racing: true, race: Race.last).sort_by(&:last_name).map {|rider| "#{rider.bib} - #{rider.last_name} #{rider.first_name}"}
   end
 
   def self.get_teams
     teams = []
-    Rider.where(race: Race.first).map do |rider|
+    Rider.where(race: Race.last).map do |rider|
       teams << "#{rider.team}" unless teams.include?(rider.team)
     end
     return teams.sort
