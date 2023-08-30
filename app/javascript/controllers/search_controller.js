63278c67ded2525330b0b2b4893ed1911875cf2d
cedric le brun <lebruncedric8@gmail.com>
2023-08-30 08:34:01 +0200

    Try to fix bug with select2 in production

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,32 +2,28 @@ import { Controller } from "@hotwired/stimulus";
 import "select2";
 
 export default class extends Controller {
-  static targets = ["winner", "second", "third"]
+  static targets = ["winner", "second", "third"];
 
-  initialize() {
-    console.log(this.winnerTarget.querySelector('select'));
-    this.select2Element = this.winnerTarget.querySelector('select');
+  connect() {
     this.selectWinner();
-    this.select2Element = this.secondTarget.querySelector('select');
     this.selectSecond();
-    this.select2Element = this.thirdTarget.querySelector('select');
     this.selectThird();
   }
 
   selectWinner() {
-    $(this.select2Element).select2({
+    $(this.winnerTarget.querySelector('select')).select2({
       placeholder: 'Vainqueur',
     });
   }
 
   selectSecond() {
-    $(this.select2Element).select2({
+    $(this.secondTarget.querySelector('select')).select2({
       placeholder: 'Deuxième',
     });
   }
 
   selectThird() {
-    $(this.select2Element).select2({
+    $(this.thirdTarget.querySelector('select')).select2({
       placeholder: 'Troisième',
     });
   }
