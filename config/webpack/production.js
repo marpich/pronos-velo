b3c3f5aba650eeb356028222bba3c55cda7a374c
cedric le brun <lebruncedric8@gmail.com>
2023-08-30 08:07:14 +0200

    Change production.js for jquery

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,5 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
+const webpack = require('webpack');
+
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+}));
 
 module.exports = environment.toWebpackConfig()
