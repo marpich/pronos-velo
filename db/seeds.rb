3a575b25814ab94ba03fe141173084774c1eb9e9
cedric le brun <lebruncedric8@gmail.com>
2024-06-27 22:26:30 +0200

    Modif seed pour prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,39 +5,39 @@ require 'net/http'
 require 'openssl'
 require 'active_support/inflector'
 
-# filepath = "db/fixtures/tour_stages.csv"
+filepath = "db/fixtures/tour_stages.csv"
 filepath_riders = "db/fixtures/tour_riders.csv"
 
-# puts "Creating the races"
+puts "Creating the races"
 
-# race1 = Race.new(
-#   name: 'Tour de France 2024',
-#   year: 2024,
-#   starting_date: '29/06/2024',
-#   ending_date: '21/07/2024'
-# )
-# race1.save!
+race1 = Race.new(
+  name: 'Tour de France 2024',
+  year: 2024,
+  starting_date: '29/06/2024',
+  ending_date: '21/07/2024'
+)
+race1.save!
 
-# puts "Creating races done"
+puts "Creating races done"
 
-# puts "Creating the stages"
+puts "Creating the stages"
 
-# CSV.foreach(filepath, headers: :first_row) do |row|
-#   image_map = URI.open(row['image_map'])
-#   stage = Stage.new(
-#     number: row[0],
-#     date: row['date'],
-#     stage_type: row['stage_type'],
-#     length: row['length'],
-#     departure: row['departure'],
-#     arrival: row['arrival']
-#   )
-#   stage.image.attach(io: image_map, filename: "stage#{row[0]}.png", content_type: 'image/png')
-#   stage.race = race1
-#   stage.save!
-# end
+CSV.foreach(filepath, headers: :first_row) do |row|
+  image_map = URI.open(row['image_map'])
+  stage = Stage.new(
+    number: row[0],
+    date: row['date'],
+    stage_type: row['stage_type'],
+    length: row['length'],
+    departure: row['departure'],
+    arrival: row['arrival']
+  )
+  stage.image.attach(io: image_map, filename: "stage#{row[0]}.png", content_type: 'image/png')
+  stage.race = race1
+  stage.save!
+end
 
-# puts "Creating stages done"
+puts "Creating stages done"
 
 puts "Creating the riders"
 CSV.foreach(filepath_riders, headers: :first_row) do |row|
